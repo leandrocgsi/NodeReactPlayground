2b075b5080ec7e79310ecbf62ddd320a7ae3ae6b
leandrocgsi <leandrocgsi@gmail.com>
2020-11-04 12:20:22 -0300

    Backend is done

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,59 +1,13 @@
 const express = require('express');
 
-const connection = require('./database/connection');
+const BookController = require('./controllers/BookController')
 
 const routes = express.Router();
 
-routes.get('/book', async (request, response) => {
-    const books = await connection('book').select('*');
-    return response.json(books);
-});
-
-
-routes.get('/book/:id', async (request, response) => {
-    const id = request.params.id;   
-    const book = await connection('book').where('id', id).first();
-    return response.json(book);
-});
-
-routes.post('/book', async (request, response) => {
-    const {title, author, price, launchDate} = request.body;
-
-    const id = await connection('book').insert({
-            author,
-            title,
-            launchDate,
-            price,
-        }).then(row => {return row[0]});;   
-    
-    const book = await connection('book').where('id', id).first();
-
-    return response.json(book);
-});
-
-routes.put('/book', async (request, response) => {
-    const {id, title, author, price, launchDate} = request.body;
-
-    await connection('book')    
-        .where('id', id)
-        .update({
-            author,
-            title,
-            launchDate,
-            price,
-        });
-    
-    const book = await connection('book').where('id', id).first();
-
-    return response.json(book);
-});
-
-routes.delete('/book/:id', async (request, response) => {
-    const id = request.params.id;
-    await connection('book')    
-        .where('id', id)
-        .delete();
-    return response.json();
-});
+routes.get('/book', BookController.index);
+routes.get('/book/:id', BookController.findById);
+routes.post('/book', BookController.create);
+routes.put('/book', BookController.update);
+routes.delete('/book/:id', BookController.delete);
 
 module.exports = routes;
