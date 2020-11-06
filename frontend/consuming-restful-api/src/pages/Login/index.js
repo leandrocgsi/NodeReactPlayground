bea4ac5319084887688749dd8be16f3d38fa4d94
leandrocgsi <leandrocgsi@gmail.com>
2020-11-06 17:51:47 -0300

    Consuming API with React

--- frontend/consuming-restful-api/src/pages/Login/index.js
+++ frontend/consuming-restful-api/src/pages/Login/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory} from 'react-router-dom'
+
+import api from '../../services/api'
 
 import './styles.css';
 
@@ -6,16 +9,52 @@ import logoImage from '../../assets/logo.svg';
 import padlock from '../../assets/padlock.png';
 
 export default function Login() {
+    
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const history = useHistory();
+
+    async function login(e) {
+        e.preventDefault();
+
+        const data = {
+            userName,
+            password,
+        };
+
+        try {
+            const response = await api.post('/api/auth/v1/signin', data);
+
+            localStorage.setItem('username', userName);
+            localStorage.setItem('accessToken', response.data.accessToken);
+            localStorage.setItem('refreshToken', response.data.refreshToken);
+
+            history.push('/books')
+        } catch (err) {
+            alert('Login failed! Try again!');
+        }
+    }
+
     return (
         <div className="login-container">
             <section className="form">
                 <img src={logoImage} alt="Erudio"/>
 
-                <form>
+                <form onSubmit={login}>
                     <h1>Access your Account</h1>
 
-                    <input placeholder="Username"/>
-                    <input type="password" placeholder="Password"/>
+                    <input
+                        placeholder="Username"
+                        value={userName}
+                        onChange={e => setUserName(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
 
                     <button className="button" type="submit">Login</button>
                 </form>
