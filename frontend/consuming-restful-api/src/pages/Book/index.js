931d65d9b149ed6ef404cd6ce6fd24b1b36f7bc1
leandrocgsi <leandrocgsi@gmail.com>
2020-11-05 22:27:16 -0300

    Consuming API with React

--- frontend/consuming-restful-api/src/pages/Book/index.js
+++ frontend/consuming-restful-api/src/pages/Book/index.js
@@ -1,7 +1,47 @@
 import React from 'react';
 
-import './styles.css';
+import logoImage from '../../assets/logo.svg';
+import { Link } from 'react-router-dom'
+import { FiPower, FiTrash2, FiEdit } from 'react-icons/fi';
+import './styles.css'
 
 export default function Book() {
-    return <h1>Book</h1>
+
+    return (
+        <div className="book-container">
+            <header>
+                <img src={logoImage} alt="Erudio" />
+                <span>Welcome, <strong>Leandro</strong>!</span>
+
+                <Link className="button" to="book/new">Add New Book</Link>
+                <button type="button">
+                    <FiPower size={18} color="#251FC5" />
+                </button>
+            </header>
+
+            <h1>Registered Books</h1>
+
+            <ul>
+                <li>
+                    <strong>Title:</strong>
+                    <p>Docker Deep Dive</p>
+                    <strong>Author:</strong>
+                    <p>Nigel Poulton</p>
+                    <strong>Price:</strong>
+                    <p>R$ 47,9</p>
+                    <strong>Release Date:</strong>
+                    <p>12/07/2017</p>
+
+
+                    <button type="button">
+                        <FiEdit size={20} color="#251FC5" padding-right="25px"/>
+                    </button>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#251FC5" />
+                    </button>
+                </li>                
+            </ul>
+        </div>
+    );
 }
--- frontend/consuming-restful-api/src/routes.js
+++ frontend/consuming-restful-api/src/routes.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Login from './pages/Login';
 import Book from './pages/Book';
+import NewBook from './pages/NewBook';
 
 export default function Routes(){
     return (
@@ -10,6 +11,7 @@ export default function Routes(){
             <Switch>
                 <Route path="/" exact component={Login}/>
                 <Route path="/book" component={Book}/>
+                <Route path="/books/new" component={NewBook}/>
             </Switch>
         </BrowserRouter>
     );
