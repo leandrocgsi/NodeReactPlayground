1dd5e12aa22da6a022c8d0d80e107a8c853245b4
leandrocgsi <leandrocgsi@gmail.com>
2020-11-08 23:55:04 -0300

    Consuming API with React

--- frontend/src/pages/NewBook/index.js
+++ frontend/src/pages/NewBook/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory, useParams } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi';
 
 import api from '../../services/api'
@@ -9,14 +9,48 @@ import './styles.css';
 import logoImage from '../../assets/logo.svg';
 
 export default function NewBook() {
+const [id, setId] = useState(null);
     const [author, setAuthor] = useState('');
     const [title, setTitle] = useState('');
     const [launchDate, setLaunchDate] = useState('');
     const [price, setPrice] = useState('');
 
+    const { bookId } = useParams();
+
+    const accessToken = localStorage.getItem('accessToken');
+    
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    };
+
     const history = useHistory();
 
-    async function createNewBook(e) {
+    
+    useEffect(() => {
+        if(bookId === '0') return
+        else loadBook();      
+    });
+
+    async function loadBook() {
+        try {
+            const response = await api.get(`api/book/v1/${bookId}`, authorization);
+
+            let adjustedDate = response.data.launchDate.split("T", 10)[0]
+
+            setId(response.data.id);
+            setTitle(response.data.title);
+            setAuthor(response.data.author);
+            setPrice(response.data.price);
+            setLaunchDate(adjustedDate);
+        } catch (err) {
+            alert('Error recovering Book! Try Again.');
+            history.push('/books');
+        }
+    }
+
+    async function saveOrUpdateBook(e) {
         e.preventDefault();
 
         const data = {
@@ -24,21 +58,19 @@ export default function NewBook() {
             author,
             launchDate,
             price,
-        };
-
-        
-        const accessToken = localStorage.getItem('accessToken');
+        }; 
 
         try {
-            await api.post('api/book/v1', data, {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`
-                }
-            });
+            if(bookId === '0') {
+                await api.post('api/book/v1', data, authorization);
+            } else {
+                data.id = id; 
+                await api.put('api/book/v1', data, authorization);
+            }
+            history.push('/books')
         } catch (err) {
             alert('Error while recording Book! Try Again.')
         }
-        history.push('/books')
     }
 
     return (
@@ -46,15 +78,15 @@ export default function NewBook() {
             <div className="content">
                 <section className="form">
                     <img src={logoImage} alt="Erudio"/>
-                    <h1>Add New Book</h1>
-                    <p>Enter the book information and click on 'Add'!</p>
-                    <Link className="back-link" to="/book">
+                    <h1>{bookId === '0' ? 'Add New' : 'Update'} Book</h1>
+                    <p>Enter the book information and click on {bookId === '0' ? "'Add'" : "'Update'!  ####"} {id}</p>
+                    <Link className="back-link" to="/books">
                         <FiArrowLeft size={16} color="#251FC5" />
-                        Home
+                        Back to Books
                     </Link>
                 </section>
 
-                <form onSubmit={createNewBook}>
+                <form onSubmit={saveOrUpdateBook}>
                     <input
                         placeholder="Title"
                         value={title}
@@ -70,13 +102,14 @@ export default function NewBook() {
                         value={launchDate}
                         onChange={e => setLaunchDate(e.target.value)}
                     />
+
                     <input
                         placeholder="Price"
                         value={price}
-                        onChange={e => setPrice(e.target.value)}
+                        onChange={e => setPrice(e.target.value.replace(",", "."))}
                     />
 
-                    <button className="button" type="submit">Add</button>
+                    <button className="button" type="submit">{bookId === '0' ? 'Add' : 'Update'}</button>
 
                 </form>
             </div>
